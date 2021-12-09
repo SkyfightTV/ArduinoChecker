393dbe6dec79086b76deb864c791223c4359c3d3
SkyfightTV <antoinecraft.fr@gmail.com>
2021-12-09 19:27:26 +0100

    Add photoresistor and pin type

--- src/AChecker.cpp
+++ src/AChecker.cpp
@@ -1,24 +1,36 @@
 #include "AChecker.h"
 #include "../lib/dht11/dht11.h"
 
-Result check_DHT_11(int pin, bool debug);
+Result checkDHT11(int pin, bool debug);
+
+Result checkPhotoresistor(int pin, bool debug);
 
 Result check(TYPE type, int pin, bool debug) {
-    if (pin==0)
-        return Result(PIN_NULL, pin, {});
+    if (pin == 0)
+        return Result(PIN_NULL, static_cast<const PIN_TYPE>(NULL), pin, {});
     switch (type) {
         case DHT_11:
-            return check_DHT_11(pin,debug);
+            return checkDHT11(pin, debug);
+        case PHOTORESISTOR:
+            return checkPhotoresistor(pin, debug);
         default:
-            return Result(TYPE_NULL,pin,{});
+            return Result(TYPE_NULL, static_cast<const PIN_TYPE>(NULL), pin, {});
     }
 }
 
-Result check_DHT_11(int pin, bool debug) {
+Result checkDHT11(int pin, bool debug) {
     dht11 dht{};
     dht.read(pin);
-    int values[]={dht.temperature,dht.humidity};
-    if(dht.temperature==-999||dht.humidity==-999)
-        return Result(FAILED,pin,values);
-    return Result(SUCCESS,pin,values);
+    int values[] = {dht.temperature, dht.humidity};
+    if (values[0] == -999 || values[1] == -999)
+        return Result(FAILED, DIGITAL, pin, values);
+    return Result(SUCCESS, DIGITAL, pin, values);
+}
+
+Result checkPhotoresistor(int pin, bool debug) {
+    int value = analogRead(pin);
+    int values[] = {value};
+    if (values[0] == -999)
+        return Result(FAILED, ANALOG, pin, values);
+    return Result(SUCCESS, ANALOG, pin, values);
 }
--- src/AChecker.h
+++ src/AChecker.h
@@ -2,31 +2,42 @@
 #include <algorithm>
 
 enum TYPE {
-    DHT_11
+    DHT_11,
+    PHOTORESISTOR
+};
+
+enum PIN_TYPE {
+    ANALOG,
+    DIGITAL
 };
 
 enum RESULT_TYPE {
-    SUCCESS=HIGH,
-    FAILED=LOW,
-    PIN_NULL=LOW,
-    TYPE_NULL=LOW,
-    UNKNOWN=LOW
+    SUCCESS = HIGH,
+    FAILED = LOW,
+    PIN_NULL = LOW,
+    TYPE_NULL = LOW
 };
 
 class Result {
     private:
-        RESULT_TYPE type=UNKNOWN;
-        int pin=0;
-        int *values={};
+        RESULT_TYPE resultType = static_cast<RESULT_TYPE>(NULL);
+        PIN_TYPE pinType = static_cast<PIN_TYPE>(NULL);
+        int pin = 0;
+        int *values = {};
     public:
-        explicit Result(const RESULT_TYPE type, const int pin, int *values) {
-            this->type=type;
-            this->pin=pin;
-            std::copy(values,values+sizeof(values),this->values);
+        explicit Result(const RESULT_TYPE resultType, const PIN_TYPE pinType, const int pin, int *values) {
+            this->resultType = resultType;
+            this->pinType = pinType;
+            this->pin = pin;
+            std::copy(values, values + sizeof(values), this->values);
         }
 
-        RESULT_TYPE getType() const {
-            return this->type;
+        RESULT_TYPE getResultType() const {
+            return this->resultType;
+        }
+
+        PIN_TYPE getPinType() const {
+            return this->pinType;
         }
 
         int getPin() const {
@@ -39,6 +50,7 @@ class Result {
 };
 
 Result check(TYPE type, int pin, bool debug);
+
 Result check(TYPE type, int pin) {
     check(type, pin, true);
 }
