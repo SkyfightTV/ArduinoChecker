2f09f1ad3e5bf1a7598ef667c1e4026c54286095
skyfighttv <antoinecraft.fr@gmail.Com>
2021-12-16 14:02:20 +0100

    Update result

---
+++ src/utils/Result.cpp
@@ -0,0 +1,58 @@
+static void copy(double *from, double *to)
+{
+    int n = length(from);
+    for (int i = 0; i < n; i++)
+        to[i] = from[i];
+}
+
+static int length(const double *s)
+{
+    int n;
+    for (int i = 0; s[i] != '\0'; i++)
+        n++;
+    return n;
+}
+
+Result::Result(RESULT_TYPE resultType, PIN_TYPE pinType, int pin, int *values)
+{
+    this->resultType = resultType;
+    this->pinType = pinType;
+    this->pin = pin;
+    copy(reinterpret_cast<double *>(values), this->values);
+}
+
+Result::Result(RESULT_TYPE resultType, PIN_TYPE pinType, int pin, float *values)
+{
+    this->resultType = resultType;
+    this->pinType = pinType;
+    this->pin = pin;
+    copy(reinterpret_cast<double *>(values), this->values);
+}
+
+Result::Result(RESULT_TYPE resultType, PIN_TYPE pinType, int pin, double *values)
+{
+    this->resultType = resultType;
+    this->pinType = pinType;
+    this->pin = pin;
+    copy(values, this->values);
+}
+
+RESULT_TYPE Result::getResultType()
+{
+    return this->resultType;
+}
+
+PIN_TYPE Result::getPinType()
+{
+    return this->pinType;
+}
+
+int Result::getPin()
+{
+    return this->pin;
+}
+
+double *Result::getValues()
+{
+    return this->values;
+}
