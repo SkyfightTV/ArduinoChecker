8bc3bf269d0b8db32a4712f3939dc11334b70fbb
skyfighttv <antoinecraft.fr@gmail.Com>
2022-01-06 16:02:46 +0100

    Update

--- src/utils/Result.cpp
+++ src/utils/Result.cpp
@@ -22,20 +22,6 @@ Result::Result(RESULT_TYPE resultType, PIN_TYPE pinType, int pin)
     this->pin = pin;
 }
 
-Result::Result(RESULT_TYPE resultType, PIN_TYPE pinType, int pin)
-{
-    this->resultType = resultType;
-    this->pinType = pinType;
-    this->pin = pin;
-}
-
-Result::Result(RESULT_TYPE resultType, PIN_TYPE pinType, int pin)
-{
-    this->resultType = resultType;
-    this->pinType = pinType;
-    this->pin = pin;
-}
-
 RESULT_TYPE Result::getResultType()
 {
     return this->resultType;
